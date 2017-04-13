d61f59cbaa272403067fb080427b36376ae8b777
Sevika Singh <s.singh10@umiami.edu>
2017-04-13 12:34:03 -0400

    correct answer test

--- js/quiz.js
+++ js/quiz.js
@@ -10,11 +10,33 @@
         $scope.percentage = 0;
         
     
+    
+    
         $http.get('../quiz_data.json').then(function(quizData){
             $scope.myQuestions = quizData.data;
             $scope.totalQuestions = $scope.myQuestions.length;
         });
         
+        
+        $scope.selectAnswer = function(qIndex, aIndex) {
+            
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            
+            if( questionState != 'answered' ){
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                
+                if( aIndex === correctAnswer ) {
+                   $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                } else {
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+                }
+                $scope.myQuestions[qIndex].questionState = 'answered';
+                
+            }
+            
+        }
         
        
         
