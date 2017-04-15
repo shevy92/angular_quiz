c997b717f426c296e043855922e9db47d4290182
Sevika Singh <s.singh10@umiami.edu>
2017-04-15 15:28:32 -0400

    quiz finished

--- js/quiz.js
+++ js/quiz.js
@@ -26,6 +26,8 @@
                 $scope.myQuestions[qIndex].selectedAnswer = aIndex;
                 var correctAnswer = $scope.myQuestions[qIndex].correct;
                 
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                
                 if( aIndex === correctAnswer ) {
                    $scope.myQuestions[qIndex].correctness = 'correct';
                     $scope.score += 1;
@@ -36,12 +38,56 @@
                 
             }
             
+            $scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(0);
+            
         }
         
-       
         
+        $scope.isSelected = function(qIndex, aIndex) {
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        }
+        
+         $scope.isCorrect = function(qIndex, aIndex) {
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
+       
+         
+         $scope.selectContinue = function() {
+             return $scope.activeQuestion += 1;
+         }
+        
+         $scope.createShareLinks = function(percentage) {
+             var url = 'http://sevika.co/angular-quiz';
+             
+             var emailLink = '<a class="btn email" href="#">Email a Friend</a>';
+             
+             var twiiterLink = '<a class="btn twitter" target="_blank" href="#">Tweet your score</a>';
+             
+             var newMarkup = emailLink + twitterLink;
+             
+             return $sce.trustAsHtml(newMarkup);
+             
+         }
         
     }]);
  
     
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
